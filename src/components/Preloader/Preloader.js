5bf9698648f7c262edf0fe039d7c96d00a7fa4a6
Voltrein-code <mr-voltrein@yandex.ru>
2021-11-08 12:04:54 +0300

    feat: add Preloader template

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React from 'react'
+import './Preloader.css'
 
-export default function Preloader() {
-  return <div className="preloader"></div>;
-}
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
+        </div>
+    )
+};
+
+export default Preloader
